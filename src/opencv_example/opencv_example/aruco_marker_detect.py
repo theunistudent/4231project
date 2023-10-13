36d2b2f7af0fd9757ece1da802812e6e07ece2f2
theunistudent <z5257559@ad.unsw.edu.au>
2023-10-13 15:07:27 +1100

    arruco message works

--- src/opencv_example/opencv_example/aruco_marker_detect.py
+++ src/opencv_example/opencv_example/aruco_marker_detect.py
@@ -1,52 +1,74 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
-from opencv_example.msg import aruco_message
-from cv_bridge import CvBridge
-import cv2
-import cv2.aruco as aruco
-from sensor_msgs.msg import Image
+# from sensor_msgs.msg import Image
+from messages.msg import ArucoMessage
+# from cv_bridge import CvBridge
+# import cv2
+# import cv2.aruco as aruco
+# from std_msgs.msg import String
 
 
 
 class MyNode(Node):
     def __init__(self):
         super().__init__('aruco_marker_detect')
-        self.create_timer(1, self.timer_callback)
+        self.create_timer(10, self.timer_callback)
         self.subscription = self.create_subscription( Image, 'image_raw', self.image_callback, 10 )
-        self.pose_publisher = self.create_publisher(aruco_message, 'marker_pose', 10)
-        self.bridge = CvBridge()
+        self.pose_publisher = self.create_publisher(ArucoMessage, 'marker_pose', 10)
+        # self.bridge = CvBridge()
 
+        # # testing things
+        # self.publisher = self.create_publisher(String, 'test', 10)
     def image_callback(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+    # def timer_callback(self):
+        # testing things
+        # msg = String()
+        # msg.data = "hello_world!"
+        # self.publisher.publish(msg)
 
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        # You can use these (x, y) coordinates of the corners
+        size = range(3)
+        msg = ArucoMessage()
+        for i in size:
+            msg.x.append (3.4)
+            msg.y.append(2.1)
+            msg.id.append(i)      # the z variable will hold the marker id number
 
-        corners, ids, _ = aruco.detectMarkers(gray, aruco.Dictionary_get(aruco.DICT_6X6_250))
+        # msg.x.append (3)
+        # msg.y.append(2)
+        # msg.id.append(1)      # the z variable will hold the marker id number
+        self.pose_publisher.publish(msg)
 
-        if ids is not None:
-            # Draw detected markers and their IDs
-            aruco.drawDetectedMarkers(cv_image, corners, ids)
 
-            # Iterate through each detected marker
-            for i in range(len(ids)):
-                # Get the corner points of the current marker
-                marker_corners = corners[i][0]
+    #     cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
 
-                # Extract coordinates of each corner (x, y)
-                for corner in marker_corners:
-                    x, y = corner[0], corner[1]
-                    # You can use these (x, y) coordinates of the corners
-                    msg = aruco_message()
-                    msg.x[i] = x
-                    msg.y[i] = y
-                    msg.id[i] = ids[i]       # the z variable will hold the marker id number
-                    self.pose_publisher.publish(msg)
+    #     gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+    #     corners, ids, _ = aruco.detectMarkers(gray, aruco.Dictionary_get(aruco.DICT_6X6_250))
+
+    #     if ids is not None:
+    #         # Draw detected markers and their IDs
+    #         aruco.drawDetectedMarkers(cv_image, corners, ids)
+
+    #         # Iterate through each detected marker
+    #         for i in range(len(ids)):
+    #             # Get the corner points of the current marker
+    #             marker_corners = corners[i][0]
+
+    #             # Extract coordinates of each corner (x, y)
+    #             for corner in marker_corners:
+    #                 x, y = corner[0], corner[1]
+    #                 # You can use these (x, y) coordinates of the corners
+    #                 msg = aruco_message()
+    #                 msg.x[i] = x
+    #                 msg.y[i] = y
+    #                 msg.id[i] = ids[i]       # the z variable will hold the marker id number
+    #                 self.pose_publisher.publish(msg)
                 
 
-            aruco.drawDetectedMarkers(cv_image, corners, ids)
-            image_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
-            self.image_publisher.publish(image_msg)
+            # aruco.drawDetectedMarkers(cv_image, corners, ids)
+            # image_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
+            # self.image_publisher.publish(image_msg)
 
 def main(args=None):
     rclpy.init(args=args)
