4fa97626c5288b99a841d9fae947faad161950c0
theunistudent <z5257559@ad.unsw.edu.au>
2023-10-24 16:23:38 +1100

    small change to path.cpp

--- src/transforms/src/path.cpp
+++ src/transforms/src/path.cpp
@@ -33,39 +33,41 @@ class PointTFBroadcaster : public rclcpp::Node
     PaperTFBroadcaster()
     : Node("PointTFBroadcaster")
     {
-      subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>( "movit_point", 10, std::bind(&PointTFBroadcaster::topic_callback, this, _1));
+      subscription_ = this->create_subscription<custom_messages::msg::RobotAction>( "Path", 10, std::bind(&PointTFBroadcaster::topic_callback, this, _1));
       tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
     }
 
   public:
-    void topic_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) const
+    void topic_callback(const custom_messages::msg::RobotAction::SharedPtr msg) const
     {
+      for (int i = 0; i < length(msg.x); i++)   // get index for global position message
+      {
+        geometry_msgs::msg::TransformStamped transform_msg;
+          // may need to change this header frame id
+          transform_msg.header.frame_id = "paper_corner_1";
+          transform_msg.child_frame_id = "next_point"
 
-      geometry_msgs::msg::TransformStamped transform_msg;
-        // may need to change this header frame id
-        transform_msg.header.frame_id = "paper_corner_1";
-        transform_msg.child_frame_id = "next_point"
+          if (!msg->poses.empty()) {
+              transform_msg.transform.translation.x = msg.x;
+              transform_msg.transform.translation.y = msg.y;
+              transform_msg.transform.translation.z = 80;     // random arbitry choice at the moment
 
-        if (!msg->poses.empty()) {
-            transform_msg.transform.translation.x = msg.pose.position.x;
-            transform_msg.transform.translation.y = msg.pose.position.y;
-            transform_msg.transform.translation.z = msg.pose.position.z;
+              tf2::Quaternion q;
+              q.setRPY(0.0, 0.0, 0.0); // -30 degrees pitch in radians
+              transform_msg.transform.rotation.x = q.x();
+              transform_msg.transform.rotation.y = q.y();
+              transform_msg.transform.rotation.z = q.z();
+              transform_msg.transform.rotation.w = q.w();
+          }
 
-            tf2::Quaternion q;
-            q.setRPY(0.0, 0.0, 0.0); // -30 degrees pitch in radians
-            transform_msg.transform.rotation.x = q.x();
-            transform_msg.transform.rotation.y = q.y();
-            transform_msg.transform.rotation.z = q.z();
-            transform_msg.transform.rotation.w = q.w();
-        }
-
-      // 
-       tf_broadcaster_->sendTransform(transform_msg);
+        // 
+        tf_broadcaster_->sendTransform(transform_msg);
+      }
     }
 
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr subscription_;
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr publisher_;
-    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_point;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
     private:
    // std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
